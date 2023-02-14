ebca260113dee80cc90bca09e732e1a1f85fb276
McLaughlan <mclaughlan@protonmail.ch>
2023-02-14 16:24:57 +0000

    Added IAM roles and policies for processing lambda.

--- terraform/iam.tf
+++ terraform/iam.tf
@@ -13,9 +13,43 @@ data "aws_iam_policy_document" "read-write-access-ingest-bucket-document" {
   }
 }
 
+data "aws_iam_policy_document" "read-only-access-ingest-bucket-document" {
+  statement {
+    sid       = "ListObjectsInBucket"
+    effect    = "Allow"
+    actions   = ["s3:Get*", "s3:List*"]
+    resources = ["arn:aws:s3:::${aws_s3_bucket.ingest-bucket.bucket}"]
+  }
+}
+
+data "aws_iam_policy_document" "read-write-access-processed-bucket-document" {
+  statement {
+    sid       = "ListObjectsInBucket"
+    effect    = "Allow"
+    actions   = ["s3:ListBucket"]
+    resources = ["arn:aws:s3:::${aws_s3_bucket.processed-bucket.bucket}"]
+  }
+  statement {
+    sid       = "AllObjectActions"
+    effect    = "Allow"
+    actions   = ["s3:*Object"]
+    resources = ["arn:aws:s3:::${aws_s3_bucket.processed-bucket.bucket}/*"]
+  }
+}
+
 resource "aws_iam_policy" "read-write-access-ingest-bucket-policy" {
   name_prefix = "read-write-access-ingest-bucket-policy"
   policy      = data.aws_iam_policy_document.read-write-access-ingest-bucket-document.json
+}
+
+resource "aws_iam_policy" "read-only-access-ingest-bucket-policy" {
+  name_prefix = "read-only-access-ingest-bucket-policy"
+  policy      = data.aws_iam_policy_document.read-only-access-ingest-bucket-document.json
+}
+
+resource "aws_iam_policy" "read-write-access-processed-bucket-policy" {
+  name_prefix = "read-write-access-processed-bucket-policy"
+  policy      = data.aws_iam_policy_document.read-write-access-processed-bucket-document.json
 }
 
 resource "aws_iam_role" "ingest-lambda-role" {
@@ -40,7 +74,39 @@ resource "aws_iam_role" "ingest-lambda-role" {
     EOF
 }
 
+resource "aws_iam_role" "processed-lambda-role" {
+  name               = "processed-lambda-role"
+  assume_role_policy = <<EOF
+    {
+        "Version": "2012-10-17",
+        "Statement": [
+            {
+                "Effect": "Allow",
+                "Action": [
+                    "sts:AssumeRole"
+                ],
+                "Principal": {
+                    "Service": [
+                        "lambda.amazonaws.com"
+                    ]
+                }
+            }
+        ]
+    }
+    EOF
+}
+
 resource "aws_iam_role_policy_attachment" "ingest-lambda-read-write-policy-attachment" {
   role       = aws_iam_role.ingest-lambda-role.name
   policy_arn = aws_iam_policy.read-write-access-ingest-bucket-policy.arn
+}
+
+resource "aws_iam_role_policy_attachment" "processed-lambda-read-only-ingest-bucket-policy-attachment" {
+  role       = aws_iam_role.processed-lambda-role.name
+  policy_arn = aws_iam_policy.read-only-access-ingest-bucket-policy.arn
+}
+
+resource "aws_iam_role_policy_attachment" "processed-lambda-read-write-processed-bucket-policy-attachment" {
+  role       = aws_iam_role.processed-lambda-role.name
+  policy_arn = aws_iam_policy.read-write-access-processed-bucket-policy.arn
 }
