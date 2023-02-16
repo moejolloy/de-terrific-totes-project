64316f3c33ca3ff25b9c5f92a2ddeda44271a423
Marie Collinge <mariecollinge@D53-MC-00614.local>
2023-02-16 12:30:24 +0000

    this adds functions for formatting the data for staff, location and design

--- src/transformation.py
+++ src/transformation.py
@@ -1,4 +1,5 @@
 import boto3
+import pandas as pd
 
 
 def transform_data(event, context):
@@ -16,3 +17,43 @@ def transform_data(event, context):
         """
 
     s3 = boto3.client('s3')
+
+
+# staff_data = pd.read_csv("test_files/test_data_staff.csv",
+#                          parse_dates=["created_at", "last_updated"])
+
+# dept_data = pd.read_csv("test_files/test_data_departments.csv",
+#                         parse_dates=["created_at", "last_updated"])
+
+# location_df = pd.read_csv(
+#     "test_files/test_address.csv", parse_dates=["created_at", "last_updated"])
+
+# design_df = pd.read_csv("test_files/test_design.csv",
+#                         parse_dates=["created_at", "last_updated"])
+
+# with pd.option_context("display.max_columns", None):
+#     print(dim_location)
+
+
+def format_dim_staff(staff_df, dept_df):
+
+    staff_dept = pd.merge(staff_df, dept_df,
+                          how='left', on="department_id")
+
+    dim_staff = staff_dept[["staff_id", "first_name", "last_name",
+                            "department_name", "location", "email_address"]]
+
+    return dim_staff
+
+
+def format_dim_location(location_df):
+    replacement_value = "None"
+    location_df["address_line_2"].fillna(replacement_value, inplace=True)
+    location_df = location_df.rename(columns={"address_id": "location_id"})
+    dim_location = location_df.iloc[:, 0:8]
+    return dim_location
+
+
+def format_dim_design(design_df):
+    return design_df[["design_id",
+                      "design_name", "file_location", "file_name"]]
