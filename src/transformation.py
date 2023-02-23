54a8a8221a5958a85ad7b0eacfce09d50dfc976b
XanderIRE <alexmrireland@gmail.com>
2023-02-23 10:39:00 +0000

    adding unique bucket names for github actions test deployment

--- src/transformation.py
+++ src/transformation.py
@@ -23,8 +23,8 @@ def transform_data(event, context):
         a dictionary with keys of the files to be uploaded
         values True if file successfully processed, else None.
     """
-    bucket = "terrific-totes-ingest-bucket-20"
-    processed_bucket = "terrific-totes-processed-bucket-20"
+    bucket = "terrific-totes-ingest-bucket-21"
+    processed_bucket = "terrific-totes-processed-bucket-21"
 
     files_list = ["staff.csv", "department.csv", "address.csv",
                   "design.csv", "counterparty.csv", "currency.csv"]
--- terraform/s3.tf
+++ terraform/s3.tf
@@ -1,9 +1,9 @@
 resource "aws_s3_bucket" "ingest-bucket" {
-  bucket = "terrific-totes-ingest-bucket-20"
+  bucket = "terrific-totes-ingest-bucket-21"
 }
 
 resource "aws_s3_bucket" "processed-bucket" {
-  bucket = "terrific-totes-processed-bucket-20"
+  bucket = "terrific-totes-processed-bucket-21"
 }
 
 
