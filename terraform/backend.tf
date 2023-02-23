4ad4d839bc8e52ece36b2b4c7b35e5b5f897d668
XanderIRE <alexmrireland@gmail.com>
2023-02-23 11:07:20 +0000

    changing bucket names and deploy script - gh deploy test

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket  = "terraform-state-bucket-totedd-21"
+    bucket  = "terraform-state-bucket-totedd-25"
     key     = "terraform/terraform.tfstate"
     region  = "us-east-1"
     encrypt = true
--- terraform/s3.tf
+++ terraform/s3.tf
@@ -1,9 +1,9 @@
 resource "aws_s3_bucket" "ingest-bucket" {
-  bucket = "terrific-totes-ingest-bucket-21"
+  bucket = "terrific-totes-ingest-bucket-25"
 }
 
 resource "aws_s3_bucket" "processed-bucket" {
-  bucket = "terrific-totes-processed-bucket-21"
+  bucket = "terrific-totes-processed-bucket-25"
 }
 
 
