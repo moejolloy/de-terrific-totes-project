0ee849c3910ff34444cc077e97a466f443ddbce5
McLaughlan <mclaughlan@protonmail.ch>
2023-02-28 13:22:24 +0000

    Bucket name changes.

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket  = "terraform-state-bucket-totedd-500"
+    bucket  = "terrific-totes-terraform-bucket-revised"
     key     = "terraform/terraform.tfstate"
     region  = "us-east-1"
     encrypt = true
